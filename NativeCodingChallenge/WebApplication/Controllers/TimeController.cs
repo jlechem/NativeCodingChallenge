6929bc16831c7344a5a58ef9fcc412473ef7da09
Justin LeCheminant <jlechem@gmail.com>
2020-07-07 19:34:04 -0600

    Adding projects

---
+++ NativeCodingChallenge/WebApplication/Controllers/TimeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        public async Task<IActionResult> GetCurrentServerTime(bool simulateError = false)
+        {
+            if( simulateError)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            else
+            {
+                return Ok(DateTime.UtcNow);
+            }
+        }
+
+    }
+}
